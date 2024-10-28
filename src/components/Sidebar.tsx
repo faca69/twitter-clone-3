8234a9e067f356eb1fd150bd08dff059537e7eed
faca69 <fnikolov123456789@gmail.com>
2024-10-28 16:02:51 +0100

    added sidebar and layout changes

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { MessageCircle, Search } from "lucide-react";
+import Link from "next/link";
+
+export default function Sidebar() {
+  return (
+    <ol className="flex flex-col gap-3 py-2 h-full ">
+      <li>
+        <Link href={"/"} className="flex items-center gap-2 text-4xl mb-5">
+          F
+        </Link>
+      </li>
+
+      <li>
+        <Link href={"/explore"} className="flex items-center gap-2">
+          <Search />
+          Explore
+        </Link>
+      </li>
+      <li>
+        <Link href={"/messages"} className="flex items-center gap-2">
+          <MessageCircle />
+          Messages
+        </Link>
+      </li>
+    </ol>
+  );
+}
