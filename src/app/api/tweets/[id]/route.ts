630a9231be6dd22b0b027349622e32521ec086f2
faca69 <fnikolov123456789@gmail.com>
2024-11-06 19:14:17 +0100

    eslint fix 5

--- src/app/api/tweets/[id]/route.ts
+++ src/app/api/tweets/[id]/route.ts
@@ -1,11 +1,11 @@
-import { getTweetById } from '../../../../services/tweets.service';
-import { NextRequest, NextResponse } from 'next/server';
+import { getTweetById } from "../../../../services/tweets.service";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-	req: NextRequest,
-	{ params: { id } }: { params: { id: string } }
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
 ) {
-	const tweet = await getTweetById(id);
+  const tweet = await getTweetById(id);
 
-	return NextResponse.json(tweet);
+  return NextResponse.json(tweet);
 }
